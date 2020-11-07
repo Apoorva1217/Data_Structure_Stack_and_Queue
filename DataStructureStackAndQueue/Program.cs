764977e48aa880848e0db31b9d0dad0f5c3c1822
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-07 12:10:23 +0530

    [Apoorva] Add . Welcome to Data Structure Stack and Queue

---
+++ DataStructureStackAndQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureStackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structure Stack and Queue!");
+        }
+    }
+}
